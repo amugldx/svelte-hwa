8db188897203a374ce4c199a51c135362e44f6fd
ammarmughal <meet.sagi.here@gmail.com>
2020-05-11 22:12:12 +0500

    html added

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,14 @@
-<h1>Test</h1>
+<script>
+  import HowTo from "./HowTo/HowTo.svelte";
+  import Timer from "./Timer/Timer.svelte";
+</script>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+</style>
+
+<h1>Handwashing App</h1>
+<Timer />
+<HowTo />
---
+++ src/Timer/Timer.svelte
@@ -0,0 +1,20 @@
+<script>
+  import ProgressBar from "./../ProgressBar/ProgressBar.svelte";
+</script>
+
+<style>
+  h2 {
+    margin: 0;
+  }
+  .start {
+    background-color: rgb(41, 185, 149);
+    width: 100%;
+    margin: 10px 0;
+  }
+</style>
+
+<div bp="grid">
+  <h3 bp="offset-5@md 4@md 12@sm">Seconds Left:</h3>
+</div>
+<ProgressBar />
+<button class="start">Start</button>
