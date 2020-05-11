bbee34b4c139fc9648a448702749b2c39d57970c
ammarmughal <meet.sagi.here@gmail.com>
2020-05-11 22:44:11 +0500

    timer code done

--- src/ProgressBar/ProgressBar.svelte
+++ src/ProgressBar/ProgressBar.svelte
@@ -1,3 +1,7 @@
+<script>
+  export let progress = 0;
+</script>
+
 <style>
   .progress-container {
     margin: 20px 0;
@@ -7,12 +11,15 @@
     height: 30px;
     background-color: rgb(77, 199, 55);
   }
+  span {
+    text-align: center;
+  }
 </style>
 
 <div bp="grid">
   <div bp="offset-5@md 4@md 12@sm" class="progress-container">
-    <div class="progress-bar" style="width: 20%">
-      <span class="sr-only">%</span>
+    <div class="progress-bar" style="width: {progress}%">
+      <span class="sr-only">{progress}%</span>
     </div>
   </div>
 </div>
--- src/Timer/Timer.svelte
+++ src/Timer/Timer.svelte
@@ -1,5 +1,23 @@
 <script>
   import ProgressBar from "./../ProgressBar/ProgressBar.svelte";
+
+  const totalSeconds = 20;
+  let secondsLeft = totalSeconds;
+  let isRunning = false;
+
+  $: progress = ((totalSeconds - secondsLeft) / totalSeconds) * 100;
+
+  const startTimer = () => {
+    isRunning = true;
+    const timer = setInterval(() => {
+      secondsLeft -= 1;
+      if (secondsLeft === 0) {
+        clearInterval(timer);
+        isRunning = false;
+        secondsLeft = totalSeconds;
+      }
+    }, 1000);
+  };
 </script>
 
 <style>
@@ -7,14 +25,28 @@
     margin: 0;
   }
   .start {
-    background-color: rgb(41, 185, 149);
+    background-color: rgb(46, 120, 204);
+    color: white;
     width: 100%;
     margin: 10px 0;
+  }
+  .start[disabled] {
+    background: rgb(194, 194, 194);
+    cursor: not-allowed;
+    color: black;
   }
 </style>
 
 <div bp="grid">
-  <h3 bp="offset-5@md 4@md 12@sm">Seconds Left:</h3>
+  <h3 bp="offset-5@md 4@md 12@sm">Seconds Left: {secondsLeft}</h3>
 </div>
-<ProgressBar />
-<button class="start">Start</button>
+<ProgressBar {progress} />
+<div bp="grid">
+  <button
+    disabled={isRunning}
+    on:click={startTimer}
+    bp="offset-5@md 4@md 12@sm"
+    class="start">
+    Start
+  </button>
+</div>
